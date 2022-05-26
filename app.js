8fa6d593eba53797b401bc0b63a789f070abba63
Pujan77 <pujansapkota7@gmail.com>
2022-05-26 10:22:01 +0545

    add switch statement

--- app.js
+++ app.js
@@ -8,13 +8,36 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res){
     
     var today = new Date();
+    currentDay = today.getDay();
     var day = "";
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        day = "Weekend";
-    } else {
-        day = "Weekday";
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Truesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log("Error: Current day = "+ currentDay);
     }
-    res.render('list', {kindOfDay: day});
+    res.render('list', {
+        kindOfDay: day
+    });
 });
 
 app.listen(3000, function(){
