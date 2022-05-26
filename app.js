769674607d59f7c8ff42cd9295b81d90814196a9
Pujan77 <pujansapkota7@gmail.com>
2022-05-26 10:16:49 +0545

    use ejs start

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
     
     var today = new Date();
+    var day = "";
     if (today.getDay() === 6 || today.getDay() === 0) {
-        res.send("It's the weekend");
+        day = "Weekend";
     } else {
-        res.sendFile(__dirname+"/index.html");
+        day = "Weekday";
     }
+    res.render('list', {kindOfDay: day});
 });
 
 app.listen(3000, function(){
