3323460b67c88877fb039a90c0c3f42e2b2eb98c
Pujan77 <pujansapkota7@gmail.com>
2022-05-26 11:17:03 +0545

    add item in todolist by end user

--- app.js
+++ app.js
@@ -2,42 +2,32 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
+var items = [];
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
     
     var today = new Date();
-    currentDay = today.getDay();
-    var day = "";
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Truesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Error: Current day = "+ currentDay);
-    }
+    
+    var options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+    var day = today.toLocaleDateString("en-US", options);
+
     res.render('list', {
-        kindOfDay: day
+        kindOfDay: day,
+        toDoList: items
     });
+});
+
+app.post('/', function(req, res){
+    var item =req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 });
 
 app.listen(3000, function(){
