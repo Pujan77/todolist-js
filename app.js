ab562dec03590224329a2eb31295629e4365cf5e
Pujan77 <pujansapkota7@gmail.com>
2022-05-27 23:22:10 +0545

    some css and more on ejs

--- app.js
+++ app.js
@@ -2,32 +2,54 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-var items = [];
+let items = [];
+let workItems = [];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(req, res){
     
-    var today = new Date();
+    let today = new Date();
     
-    var options = {
+    let options = {
         weekday: 'long',
         day: 'numeric',
         month: 'long'
     };
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
     res.render('list', {
-        kindOfDay: day,
+        listTitle: day,
         toDoList: items
     });
 });
 
+app.get("/work", function(req,res){
+    res.render("list", {listTitle: "Work List", toDoList: workItems});
+
+});
+
+// app.post("/work", function(req,res){
+//     let item = req.body.newItem;
+//     workItems.push(item);
+//     res.redirect("/work");
+// });
+
 app.post('/', function(req, res){
-    var item =req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+    let item =req.body.newItem;
+    if (req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+    
+    
+    
 });
 
 app.listen(3000, function(){
