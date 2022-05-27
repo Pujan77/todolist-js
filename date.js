e0f869fb8f2ea3ae9fa88b901d0dd2a90d1f3745
Pujan77 <pujansapkota7@gmail.com>
2022-05-27 23:43:51 +0545

    shortening js

--- date.js
+++ date.js
@@ -1,5 +1,4 @@
-module.exports.getDate = getDate;
-function getDate(){
+exports.getDate = function (){
     let today = new Date();
     
     let options = {
@@ -9,10 +8,9 @@ function getDate(){
      
 
     return today.toLocaleDateString("en-US", options);
-}
+};
 
-module.exports.getDay = getDay;
-function getDay(){
+exports.getDay = function (){
     let today = new Date();
     
     let options = {
@@ -21,4 +19,4 @@ function getDay(){
      
 
     return today.toLocaleDateString("en-US", options);
-}
+};
